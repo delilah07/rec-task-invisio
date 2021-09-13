c46b78c51961cb6a197d1d91d81f9f9c9f11b890
unknown <you@example.com>
2021-09-13 22:23:19 +0200

    init: rec-task for Invisio

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style.css";
+
+import "../index.html";
+
+import "jquery";
+import "popper.js";
+
+import "bootstrap";
+
+jQuery(document).ready(function ($) {
+  $("body").css("background-color", "orange");
+});
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./js/main.js",
+  output: {
+    filename: `./js/${filename("js")}`,
+    path: path.resolve(__dirname, "app"),
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: path.resolve(__dirname, "app"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename("css")}`,
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+};
